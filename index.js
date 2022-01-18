7f0a9cc74fc8792ca2aabd15b10301e7e62e9423
Oliver Ekwalla <oliverekwalla@gmail.com>
2022-01-18 15:53:59 +0000

    Port change

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.post('/api/persons', morgan(':method :url :status :res[content-length] - :re
 })
 
 
-const PORT = procress.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
