faaa8403d0a202656fe97532c95cb7155c4f8c55
Oliver Ekwalla <oliverekwalla@gmail.com>
2022-01-23 15:42:24 +0000

    deployment fix

--- mongo.js
+++ mongo.js
@@ -4,7 +4,7 @@ const passport = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
 
-const url = `mongodb+srv://oliverxekwalla:${password}@cluster0.6gu0p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const url = ` mongodb+srv://oliverxekwalla:${password}@cluster0.6gu0p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 mongoose.connect(url)
 
